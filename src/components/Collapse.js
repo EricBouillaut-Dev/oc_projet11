e7ca56a0c4496cc3ffccf474178dbfeb39651450
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-09-27 20:26:05 +0200

    Finish about page (collapse)

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,5 +1,24 @@
-function Collapse() {
-  return <div className="collapse"></div>;
+import React, { useState } from "react";
+
+function Collapse({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`collapse ${isOpen ? "open" : ""}`}>
+      <button className="collapse-button" onClick={toggleCollapse}>
+        {title}
+        <i
+          className={`fa fa-angle-up ${isOpen ? "rotate" : ""}`}
+          aria-hidden="true"
+        ></i>
+      </button>
+      <p className="collapse-content">{description}</p>
+    </div>
+  );
 }
 
 export default Collapse;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
 import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
 import Banner_About from "../assets/banner_About.jpg";
+import AboutData from "../datas/AboutData";
 
 function About() {
   return (
     <div>
       <Banner imagesrc={Banner_About} text="" />
-      <div className="about">{/* {TODO} */}</div>
+      <div className="about">
+        {Object.entries(AboutData).map(([key, value]) => (
+          <Collapse key={key} title={key} description={value} />
+        ))}
+      </div>
     </div>
   );
 }
