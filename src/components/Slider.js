aa3e8b47f866d0206df507c26f6cb78860cb59d3
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-10-05 11:47:27 +0200

    Corrections soutenance

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 
-//Composant slider (carrousel)
 function Slider({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  //Utilisation d'un modulo pour faire la rotation des images (max+1=min/min-1=max)
   const changeImage = (increment) => {
     setCurrentIndex(
       (index) => (index + increment + images.length) % images.length
@@ -14,12 +12,17 @@ function Slider({ images }) {
   return (
     <div className="image-slider">
       <div className="slider-image-container">
-        <img src={images[currentIndex]} alt={`${currentIndex + 1}`} />
-        <span className="slider-number">
-          {currentIndex + 1}/{images.length}
-        </span>
-        <i className="fa fa-angle-left" onClick={() => changeImage(-1)}></i>
-        <i className="fa fa-angle-right" onClick={() => changeImage(1)}></i>
+        {images.length > 1 && (
+          <>
+            <img src={images[currentIndex]} alt={`${currentIndex + 1}`} />
+            <span className="slider-number">
+              {currentIndex + 1}/{images.length}
+            </span>
+            <i className="fa fa-angle-left" onClick={() => changeImage(-1)}></i>
+            <i className="fa fa-angle-right" onClick={() => changeImage(1)}></i>
+          </>
+        )}
+        {images.length === 1 && <img src={images[0]} alt="1" />}
       </div>
     </div>
   );
